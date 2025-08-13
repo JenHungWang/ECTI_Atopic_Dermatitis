3151fc89e6c4409b2a02fe5b6141d2d8d4c037c5
JenHungWang <jenhung.wa@gmail.com>
2025-08-13 15:55:52 +0200

    update norm parameters for scn

--- utils/QC_Predictor.py
+++ utils/QC_Predictor.py
@@ -45,8 +45,8 @@ class ModelPredictor:
         transform = transforms.Compose([
             transforms.Resize((self.input_size, self.input_size)),
             transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                 std=[0.229, 0.224, 0.225])
+            transforms.Normalize(mean=[0.819, 0.444, 0.118],
+                                 std=[0.204, 0.291, 0.173])
         ])
 
         image = Image.open(image_path).convert('RGB')

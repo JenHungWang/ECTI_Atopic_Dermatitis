ef05d95e7be44bf1b7cf3b0149b55fe48d71d9cf
Aske <s230642@dtu.dk>
2025-08-20 09:33:09 +0200

    revert to local path

--- config/global_settings.py
+++ config/global_settings.py
@@ -18,13 +18,13 @@ def import_config_dict():
     """Import config dict"""
     # TODO: Change to local path
     config = configparser.ConfigParser()
-    config.read(r"C:\Users\MIDAS\Desktop\SCN_Analysis\config\path.ini")
+    config.read('config/path.ini')
     config_dict = create_config_dict(config)
 
-    config.read(r"C:\Users\MIDAS\Desktop\SCN_Analysis\config\model.ini")
+    config.read('config/model.ini')
     config_dict.update(create_config_dict(config))
 
-    config.read(r"C:\Users\MIDAS\Desktop\SCN_Analysis\config\qc.ini")
+    config.read('config/qc.ini')
     config_dict.update(create_config_dict(config))
 
     config_dict['MODEL']['conf_threshold'] = \
